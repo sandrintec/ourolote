ecdd48f3e9c09d678c2bcd37cc75a89004802d05
sandrintec <sandrinsandro11@gmail.com>
2024-04-04 19:53:28 -0300

    novas funções

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,24 @@
+import { faker } from '@faker-js/faker';
+
 describe('mudança de senha', () => {
+    
     const login = Cypress.env("username");
     const oldPassword = Cypress.env("password");
     const newPassword = Cypress.env("newPassword");
+    const nome = faker.person.fullName();
+    const cpf = faker.seed();
+    const telefone = faker.phone.imei();
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0'); 
+    const year = today.getFullYear();
+    const month2 = String(today.getMonth() + 2).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    const formattedDate2 = `${year}-${month2}-${day}`;
+    const valorEntrada = faker.datatype.number({ min: 1000, max: 5000 });
+    const qtdParcelasEntrada = faker.number.binary({ min: 1 });
+    const qtdParcelasRestante = faker.number.octal();
+
  
   
     it('mudar a senha com sucesso', () => {
@@ -16,11 +33,24 @@ describe('mudança de senha', () => {
       
     })
 
-    it('O sistema deve ser capaz de cadastrar um cliente', () => {
+    it.only('O sistema deve ser capaz de cadastrar um cliente', () => {
         cy.login(login, oldPassword);
-        cy.registerCustomer();
+        cy.registerCustomer(nome, cpf, telefone);
         
     })
+
+    it('O sistema deve ser capaz de fazer uma venda', () => {
+        cy.login(login, oldPassword);
+        cy.sellProduct(formattedDate);
+    })
+
+    it('O cliente deve ser capaz de parcelar a compra', () => {
+        cy.login(login, oldPassword);
+        cy.payinInstallments(valorEntrada, qtdParcelasEntrada, formattedDate, qtdParcelasRestante, formattedDate2);
+
+        cy.deleteSale(oldPassword);
+    })
+
 
   })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+// O sistema faz login
 Cypress.Commands.add('login', (login, password) => {
     cy.visit('https://ourolote.com.br/accounts/login/?next=/');
     cy.get('#id_username').type(login);
@@ -7,6 +8,7 @@ Cypress.Commands.add('login', (login, password) => {
     cy.get('.alert-success').should('not.exist');
   })
   
+  // O sistema faz a mudança de senha da antiga para uma nova
   Cypress.Commands.add('changePassword', (oldPassword, newPassword) => {
     cy.visit('https://ourolote.com.br/');
     //alterar a senha
@@ -20,9 +22,9 @@ Cypress.Commands.add('login', (login, password) => {
       
 
   })
-  
+  // O sistema fa logout
   Cypress.Commands.add('logout', () => {
-    //fazer logout
+    
     cy.get('.text-gray-600').click();
     cy.get('[href="/accounts/logout/"]').click();
     cy.get('.alert-success').should('not.exist');
@@ -30,7 +32,8 @@ Cypress.Commands.add('login', (login, password) => {
 
 
   })
-
+  
+  // O sistema faz a mudança de senha da nova para a antiga
   Cypress.Commands.add('changePassword2', (newPassword, oldPassword) => {
 
     cy.get('.text-gray-600').click();
@@ -43,15 +46,49 @@ Cypress.Commands.add('login', (login, password) => {
   
   })
 
-  const { faker } = require('@faker-js/faker');
   
-
-  Cypress.Commands.add('registerCustomer', () => {
+  // O sistema faz o cadastro de um cliente
+  Cypress.Commands.add('registerCustomer', (nome, cpf, telefone) => {
     cy.visit('https://ourolote.com.br/clientes/');
     cy.get('[href="/clientes/"]').click();
     cy.get('[href="/cliente/create/"]').click();
-    cy.get('[name="nome"]').type(faker.person.fullName());
-    cy.get('[name="cpf"]').type(faker.seed());
-    cy.get('[name="telefone"]').type(faker.phone.imei());
+    cy.get('[name="nome"]').type(nome);
+    cy.get('[name="cpf"]').type(cpf);
+    cy.get('[name="telefone"]').type(telefone);
     //cy.get('[type="submit"]').click();
+  })
+
+
+    // O sistema faz a venda de um produto
+    Cypress.Commands.add('sellProduct', (formattedDate) => {
+    cy.visit('https://ourolote.com.br/');
+    cy.get('[href="/loteamento/8/"]').click();
+    cy.get('[href="#collapse-1"]').click();
+    cy.get('[href="/lote/359/"]').click();
+    cy.get('[href="/venda/create/359/"]').click();
+    cy.get('#select2-cliente-container').click()
+    cy.get('select#cliente').select('sandro santos', {force: true});
+    cy.get('#id_data').type(formattedDate);
+    cy.get('form > .btn > .text-white').click();
+
+  })
+  // O sistema faz um parcelamento da venda
+  Cypress.Commands.add('payinInstallments', (valorEntrada, qtdParcelasEntrada, formattedDate, qtdParcelasRestante, formattedDate2) => {
+    cy.visit('https://ourolote.com.br/lote/359/');
+    cy.get('.text-white-50').click();
+    cy.get('[name="valor_entrada"]').type(valorEntrada);
+    cy.get('[name="qtd_parcelas_entrada"]').type(qtdParcelasEntrada);
+    cy.get('[name="data_primeiro_pagamento_entrada"]').type(formattedDate);
+    cy.get('[name="qtd_parcelas_restantes"]').type(qtdParcelasRestante)
+    cy.get('[name="data_primeira_parcela_restante"]').type(formattedDate2);
+    cy.get('form > .btn > .text-white').click();
+
+    cy.wait(20000);
+    
+  })
+  // O sistema exclui a venda
+  Cypress.Commands.add('deleteSale', (oldPassword) => {
+    cy.get('.card-body > .btn > .text-white').click();
+    cy.get('.form-control').type(oldPassword);
+    cy.get('[type="submit"]').click();
   })
